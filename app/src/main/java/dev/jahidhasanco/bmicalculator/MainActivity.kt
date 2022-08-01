f49bb6989adf745d3085d0c0526a6c23031d63e5
Jahid Hasan <Vdjsovaj@gmail.com>
2022-08-01 21:53:43 +0600

    Weight Picker

--- app/src/main/java/dev/jahidhasanco/bmicalculator/MainActivity.kt
+++ app/src/main/java/dev/jahidhasanco/bmicalculator/MainActivity.kt
@@ -1,11 +1,61 @@
 package dev.jahidhasanco.bmicalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.SnapHelper
+import com.sccomponents.gauges.library.ScArcGauge
+import dev.jahidhasanco.bmicalculator.databinding.ActivityMainBinding
+import dev.jahidhasanco.bmicalculator.presentation.adapter.WeightPickerAdapter
+import travel.ithaka.android.horizontalpickerlib.PickerLayoutManager
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    val _binding get() = binding
+
+    lateinit var weightAdapter:WeightPickerAdapter
+
+    private val mAngle = 1f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        _binding.genderMeter.angleStart = mAngle
+
+        val pickerLayoutManager = PickerLayoutManager(this, PickerLayoutManager.HORIZONTAL, false)
+        pickerLayoutManager.apply {
+            isChangeAlpha = true
+            scaleDownBy = 0.99f
+            scaleDownDistance = 0.8f
+        }
+
+
+        val snapHelper: SnapHelper = LinearSnapHelper()
+        snapHelper.attachToRecyclerView(_binding.weightRecyclerBtn)
+
+        weightAdapter = WeightPickerAdapter(this, getData(100), _binding.weightRecyclerBtn)
+
+        _binding.weightRecyclerBtn.apply {
+            layoutManager = pickerLayoutManager
+            adapter = weightAdapter
+        }
+
+
+        pickerLayoutManager.setOnScrollStopListener {
+
+        }
+
+    }
+
+    fun getData(count: Int): List<String> {
+        val data: MutableList<String> = ArrayList()
+        for (i in 0 until count) {
+            data.add(i.toString())
+        }
+        return data
     }
 }
---
+++ app/src/main/java/dev/jahidhasanco/bmicalculator/presentation/adapter/WeightPickerAdapter.kt
@@ -0,0 +1,50 @@
+package dev.jahidhasanco.bmicalculator.presentation.adapter
+
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import dev.jahidhasanco.bmicalculator.R
+
+
+class WeightPickerAdapter(private val context: Context, private var dataList: List<String>, private val recyclerView: RecyclerView) :
+    RecyclerView.Adapter<WeightPickerAdapter.TextVH>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextVH {
+        val view: View
+        val inflater = LayoutInflater.from(context)
+        view = inflater.inflate(R.layout.weight_picker_item_layout, parent, false)
+        return TextVH(view)
+    }
+
+    override fun onBindViewHolder(holder: TextVH, position: Int) {
+
+        holder.pickerTxt.text = dataList[position]
+        holder.pickerTxt.setOnClickListener {
+            recyclerView.smoothScrollToPosition(position)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    fun swapData(newData: List<String>) {
+        dataList = newData
+        notifyDataSetChanged()
+    }
+
+     class TextVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var pickerTxt: TextView
+
+        init {
+            pickerTxt = itemView.findViewById(R.id.weight_picker_item)
+        }
+    }
+
+}
