c569ff19bc2367ce9ce8b48e53837a85ed694081
Jahid Hasan <Vdjsovaj@gmail.com>
2022-08-02 08:40:55 +0600

    gender ui created

--- app/src/main/java/dev/jahidhasanco/bmicalculator/MainActivity.kt
+++ app/src/main/java/dev/jahidhasanco/bmicalculator/MainActivity.kt
@@ -1,14 +1,16 @@
 package dev.jahidhasanco.bmicalculator
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.SnapHelper
-import com.sccomponents.gauges.library.ScArcGauge
 import dev.jahidhasanco.bmicalculator.databinding.ActivityMainBinding
 import dev.jahidhasanco.bmicalculator.presentation.adapter.WeightPickerAdapter
 import travel.ithaka.android.horizontalpickerlib.PickerLayoutManager
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,13 +20,23 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var weightAdapter:WeightPickerAdapter
 
-    private val mAngle = 1f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        _binding.genderMeter.angleStart = mAngle
+        val titlesofGender: List<String> = listOf("M","O", "F")
+
+        _binding.genderWheelView.apply {
+            titles = titlesofGender
+            elevation = 0f
+        }
+        _binding.genderWheelView.selectListener =  {
+            Toast.makeText(this@MainActivity,"Click on WheelView" + titlesofGender[it] ,Toast.LENGTH_SHORT).show()
+        }
+
+
+
 
         val pickerLayoutManager = PickerLayoutManager(this, PickerLayoutManager.HORIZONTAL, false)
         pickerLayoutManager.apply {
@@ -37,7 +49,7 @@ class MainActivity : AppCompatActivity() {
         val snapHelper: SnapHelper = LinearSnapHelper()
         snapHelper.attachToRecyclerView(_binding.weightRecyclerBtn)
 
-        weightAdapter = WeightPickerAdapter(this, getData(100), _binding.weightRecyclerBtn)
+        weightAdapter = WeightPickerAdapter(this, getData(102), _binding.weightRecyclerBtn)
 
         _binding.weightRecyclerBtn.apply {
             layoutManager = pickerLayoutManager
