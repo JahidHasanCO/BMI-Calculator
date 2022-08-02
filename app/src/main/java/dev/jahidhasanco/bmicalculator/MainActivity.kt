b0b409b1edd0575ba6891e679420554bf9293042
Jahid Hasan <Vdjsovaj@gmail.com>
2022-08-02 12:36:57 +0600

    update

--- app/src/main/java/dev/jahidhasanco/bmicalculator/MainActivity.kt
+++ app/src/main/java/dev/jahidhasanco/bmicalculator/MainActivity.kt
@@ -2,6 +2,7 @@ package dev.jahidhasanco.bmicalculator
 
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -10,7 +11,6 @@ import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.SnapHelper
 import com.cncoderx.wheelview.OnWheelChangedListener
 import dev.jahidhasanco.bmicalculator.databinding.ActivityMainBinding
-
 import dev.jahidhasanco.bmicalculator.presentation.adapter.WeightPickerAdapter
 import travel.ithaka.android.horizontalpickerlib.PickerLayoutManager
 import java.util.*
@@ -28,11 +28,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        val titlesofGender: List<String> = listOf("M","O", "F")
+        val titlesofGender: List<String> = listOf("F","O", "M")
+
 
         _binding.genderWheelView.apply {
             titles = titlesofGender
             elevation = 0f
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                isFocusedByDefault = true
+            }
+            isSelected = true
+            focusedIndex = 2
         }
         _binding.genderWheelView.selectListener =  {
             Toast.makeText(this@MainActivity,"Click on WheelView" + titlesofGender[it] ,Toast.LENGTH_SHORT).show()
@@ -46,17 +52,32 @@ class MainActivity : AppCompatActivity() {
             isChangeAlpha = true
             scaleDownBy = 0.99f
             scaleDownDistance = 0.8f
+            initialPrefetchItemCount = 10
         }
 
 
         val snapHelper: SnapHelper = LinearSnapHelper()
         snapHelper.attachToRecyclerView(_binding.weightRecyclerBtn)
 
-        weightAdapter = WeightPickerAdapter(this, getData(102), _binding.weightRecyclerBtn)
+        weightAdapter = WeightPickerAdapter(this, getData(101), _binding.weightRecyclerBtn)
+        weightAdapter.notifyDataSetChanged()
+        _binding.weightRecyclerBtn.smoothScrollToPosition(70)
 
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            _binding.weightRecyclerBtn.defaultFocusHighlightEnabled = true
+        }
         _binding.weightRecyclerBtn.apply {
             layoutManager = pickerLayoutManager
             adapter = weightAdapter
+            isSelected = true
+            requestFocus()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                isFocusedByDefault = true
+            }
+
+
         }
 
 
@@ -68,9 +89,16 @@ class MainActivity : AppCompatActivity() {
             OnWheelChangedListener { view, oldIndex, newIndex ->
                 val text = view.getItem(newIndex)
                 Log.i("WheelView", String.format("index: %d, text: %s", newIndex, text))
-            }
+
+                }
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            _binding.heightWheel.defaultFocusHighlightEnabled = true
+            _binding.heightWheel.apply {
+                defaultFocusHighlightEnabled = true
+//                isFocusedByDefault = true
+//                isSelected = true
+//                focusSearch(View.FOCUS_DOWN)
+            }
         }
 
 
@@ -83,6 +111,7 @@ class MainActivity : AppCompatActivity() {
         }
         return data
     }
+
 
 
 }
