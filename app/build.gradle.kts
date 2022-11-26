8863eb87a66f4b0bf297fe4eef23116dec5cf105
Md. Zahidul Islam <50579963+JahidHasanCO@users.noreply.github.com>
2022-11-26 06:26:22 +0600

    Fix #17 versionName & versionCode not updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.jahidhasanco.bmicalculator"
         minSdk = 21
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
