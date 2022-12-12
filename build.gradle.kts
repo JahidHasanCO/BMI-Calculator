501c2077951687a65c35788c7acc8f1a95312c1c
Md. Zahidul Islam <50579963+JahidHasanCO@users.noreply.github.com>
2022-12-12 03:37:32 +0000

    update sonarcloud

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    dependencies {
+        classpath("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:2.7.1")
+    
+    }
+}
+
 plugins {
     id("com.android.application") version "7.3.1" apply false
     id("com.android.library") version "7.3.1" apply false
     id("org.jetbrains.kotlin.android") version "1.7.20" apply false
+    id("org.sonarsource.scanner.gradle") version "2.7.1" apply false
 }
 
 tasks.register("clean",Delete::class){
