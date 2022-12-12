7dbee2bdcfc9988dcf7e4564a199c1fd49ab9c8b
Md. Zahidul Islam <50579963+JahidHasanCO@users.noreply.github.com>
2022-12-12 04:01:25 +0000

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,15 @@ plugins {
     id("com.android.application") version "7.3.1" apply false
     id("com.android.library") version "7.3.1" apply false
     id("org.jetbrains.kotlin.android") version "1.7.20" apply false
+    id("org.sonarqube") version "3.5.0.2730"
+}
 
+sonarqube {
+  properties {
+    property("sonar.projectKey", "JahidHasanCO_BMI-Calculator")
+    property("sonar.organization", "jahidhasanco")
+    property("sonar.host.url", "https://sonarcloud.io")
+  }
 }
 
 tasks.register("clean",Delete::class){
