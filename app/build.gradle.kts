0f473fce8980082d2d1419bbdd5ad0919463bf80
jahid <vdjsovaj@gmail.com>
2023-03-18 15:19:27 +0600

    update color

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.jahidhasanco.bmicalculator"
         minSdk = 21
         targetSdk = 33
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
